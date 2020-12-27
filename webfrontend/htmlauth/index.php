ab40e6a63dbfbe5e9b6fe89ca119a25150f84523
SirRules <sirrules@kwiotec.de>
2020-12-27 23:41:15 +0100

    include three.js into index.php

--- webfrontend/htmlauth/index.php
+++ webfrontend/htmlauth/index.php
@@ -4,5 +4,15 @@
  </head>
  <body>
  <?php echo '<p>Hallo Welt</p>'; ?>
+
+ <link type="text/css" rel="stylesheet" href="three/main.css">
+<div oc-lazy-load="'three/T.directive.js'">
+	<three-js-3d
+      style="position: absolute; left: 0; right: 0; top: 0; bottom: 0"
+      lib-url="static/three">
+	</three-js-3d>
+</div>
+
+
  </body>
 </html>
